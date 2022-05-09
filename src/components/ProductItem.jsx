bae2860af8ec28fdc303749a8ee37906f0375e5e
Carlos Zabala <czabala847@gmail.com>
2022-05-09 15:23:59 -0500

    Integrando estilos de react normal a next

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import addToCartImage from '@icons/bt_add_to_cart.svg';
+import addedToCartImage from '@icons/bt_added_to_cart.svg';
+
+import styles from '@styles/ProductItem.module.scss';
+
+const ProductItem = ({ product }) => {
+  const { state, addToCart } = useContext(AppContext);
+
+  const handleClick = (item) => {
+    console.log('in cart: ', state.cart.includes(item));
+    addToCart(item);
+  };
+
+  return (
+    <div className={styles.ProductItem}>
+      <img src={product.images[0]} alt={product.title} />
+      <div className="product-info">
+        <div>
+          <p>${product.price}</p>
+          <p>{product.title}</p>
+        </div>
+        <figure className="more-clickable-area" onClick={() => handleClick(product)}>
+          {state.cart.includes(product) ? (
+            <img className="disabled add-to-cart-btn" src={addedToCartImage} alt="added to cart" />
+          ) : (
+            <img className="add-to-cart-btn pointer" src={addToCartImage} alt="add to cart" />
+          )}
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
