21c46d904c5fdad0cb9e8b38fd7800e086b1791b
Carlos Zabala <czabala847@gmail.com>
2022-05-11 17:11:43 -0500

    Añadiendo custom document para el SEO

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <title>React Platzi Store</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
